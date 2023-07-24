468c9a4f9825d60314f9c671ab89d09906f39d01
runit.misra_inf <runit.misra_inf@npci.org.in>
2023-07-24 19:41:38 +0530

    added toggleterm and some keymaps

--- init.lua
+++ init.lua
@@ -282,6 +282,30 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Keymaps for Neo-Tree
 vim.keymap.set('n', '<leader>n', ":Neotree toggle<CR>", { silent = true, desc = "Toggle Neotree" })
 
+-- Keymaps for ToggleTerm
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- Keymap for opening lazygit
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({cmd = 'lazygit', hidden = true})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.keymap.set('n', '<leader>l', "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
